caaf450e20ad398a22e3bec3065105345556c280
limzhengyang <>
2025-07-27 15:05:34 +0800

    feature: add var and cvar

--- app/mote_carlo/simulation.py
+++ app/mote_carlo/simulation.py
@@ -25,7 +25,7 @@ def mc_cvar(returns: pd.Series, alpha=5):
     else:
         raise TypeError("Expected a pandas data series.")
 
-def mc_simulation(ticker: str) -> np.ndarray:
+def mc_simulation(ticker: str) -> dict:
     end_date: datetime = datetime.datetime.now()
     start_date: datetime = end_date - datetime.timedelta(days=300)
 
@@ -35,34 +35,26 @@ def mc_simulation(ticker: str) -> np.ndarray:
     weights /= np.sum(weights)
 
     # Monte Carlo Method
-    mc_sims = 400  # number of simulations
-    T = 100  # timeframe in days
-
-    mean_m: np.ndarray = np.full(shape=(T, len(weights)), fill_value=mean_returns)
-    mean_m: np.ndarray = mean_m.T
-
-    portfolio_sims: np.ndarray = np.full(shape=(T, mc_sims), fill_value=0.0)
-
+    mc_sims = 400 # number of simulations
+    T = 100 # timeframe in days
     initialPortfolio: int = 10000
 
-    for m in range(0, mc_sims):
-        Z: np.ndarray = np.random.normal(size=(T, len(weights)))  # uncorrelated RV's
-        L: np.ndarray = np.linalg.cholesky(cov_matrix)  # Cholesky decomposition to Lower Triangular Matrix
-        daily_returns: np.ndarray = mean_m + np.inner(L, Z)  # Correlated daily returns for individual stocks
+    mean_m: np.ndarray = np.full(shape=(T, len(weights)), fill_value=mean_returns).T
+    portfolio_sims: np.ndarray = np.full(shape=(T, mc_sims), fill_value=0.0)
+
+    for m in range(mc_sims):
+        Z: np.ndarray = np.random.normal(size=(T, len(weights)))
+        L: np.ndarray = np.linalg.cholesky(cov_matrix)
+        daily_returns: np.ndarray = mean_m + np.inner(L, Z)
         portfolio_sims[:, m] = np.cumprod(np.inner(weights, daily_returns.T) + 1) * initialPortfolio
 
-    # plt.plot(portfolio_sims)
-    # plt.ylabel('Portfolio Value ($)')
-    # plt.xlabel('Days')
-    # plt.title('MC simulation of a stock portfolio')
-    # plt.show()
-
-    port_results: pd.Series = pd.Series(portfolio_sims[-1,:])
+    port_results: pd.Series = pd.Series(portfolio_sims[-1, :])
 
     var: float = initialPortfolio - mc_var(port_results, alpha=5)
-    cvar:  float = initialPortfolio - mc_cvar(port_results, alpha=5)
+    cvar: float = initialPortfolio - mc_cvar(port_results, alpha=5)
 
-    print('VaR_5 ${}'.format(round(var,2)))
-    print('CVaR_5 ${}'.format(round(cvar,2)))
-
-    return portfolio_sims
+    return {
+        "simulations": portfolio_sims.tolist(),
+        "VaR_5": round(var, 2),
+        "CVaR_5": round(cvar, 2)
+    }
